8fd651afc28a45cb9685079f59e39581606caef2
LeeMinJun <dlalswns456@kakao.com>
2021-05-31 13:54:10 +0900

    99

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 
-function Food({ fav }) {
-  return <h1>I like {fav}</h1>;
-}
+class App extends React.Component{
+  state = {
+    count: 0,
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukumi" />
-    </div>
-  );
-}
+  };
 
+  add = () => {
+    console.log('add')
+  };
+
+  minus = () => {
+    console.log('minus')
+  };
+  render(){
+    return(
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
+}
 export default App;
