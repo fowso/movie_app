b16bcc11c8f5b12bdc64f9211869b93cb9e2c215
minjun's factory <59520171+fowso@users.noreply.github.com>
2021-06-03 15:18:21 +0900

    영화API사용하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-
+import axios from 'axios';
 class App extends React.Component{
   state = {
     isLoading: true,
+    movies:[],
   };
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false});
-    }, 6000);
+    axios.get('https://yts-proxy.now.sh/list_movies.json')
   }
   render(){
     const { isLoading } = this.state;
