63f400d782d70518460e8cd3e245623a94bbc260
minjun's factory <59520171+fowso@users.noreply.github.com>
2021-06-14 17:18:31 +0900

    변경사항 없음

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import axios from 'axios';
-class App extends React.Component{
+import React from "react";
+
+class App extends React.Component {
   state = {
     isLoading: true,
-    movies:[],
+    movies: [],
   };
   componentDidMount() {
-    axios.get('https://yts-proxy.now.sh/list_movies.json')
+    // 영화 데이터 로딩!
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
-  render(){
+  render() {
     const { isLoading } = this.state;
-
-    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
-
 
 export default App;
