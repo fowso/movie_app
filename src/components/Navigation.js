569589ef9bae0dd3ead0aa57553cf5d91e3f256d
minjun's factory <59520171+fowso@users.noreply.github.com>
2021-06-14 21:42:56 +0900

    a 엘리먼트 Link 컴포넌트로 바꾸기

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 function Navigation() {
     return (
         <div>
-            <a href="/">Home</a>
-            <a gref="/About">About</a>
+            <Link to="/">Home</Link>
+            <Link to="/About">About</Link>
         </div>
     );
 }
