cf8d7b46a80faaa745d00db8aa46c9fe497c57c5
minjun's factory <59520171+fowso@users.noreply.github.com>
2021-06-14 22:10:27 +0900

    location.state 확인하기

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -10,7 +10,11 @@ class Detail extends React.Component {
 
     render() {
         const { location } = this.props;
+        if (location.state) {
         return <span>{location.state.title}</span>
+        } else{
+            return null;
+        }
     }
 }
 
