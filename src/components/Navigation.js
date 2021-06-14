de6740b43524f1dd274b64e237d7061dd2228354
minjun's factory <59520171+fowso@users.noreply.github.com>
2021-06-14 22:01:05 +0900

    Movie  컴포넌트에 Link 컴포넌트 추가하기

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,8 +6,7 @@ function Navigation() {
     return (
         <div className="nav">
             <Link to="/">Home</Link>
-            <Link to={{ pathname: '/about', state: { fromNavigation: true}
-        }}>About</Link>
+            <Link to="/About">About</Link>
         </div>
     );
 }
