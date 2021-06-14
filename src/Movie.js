695bc9b6dfedac1efe60e4ad7a8e96152b2659bb
minjun's factory <59520171+fowso@users.noreply.github.com>
2021-06-14 19:07:45 +0900

    App.css 를 사용하여 배경색을 검은색으로 바꾸기

--- src/Movie.js
+++ src/Movie.js
@@ -6,7 +6,7 @@ function Movie({title, year, summary, poster}){
         <div class="movie">
             <img src={poster} alt={title} title={title}/>
         <div class="movie__data">
-            <h3 class="movie__title" style={{ backgroundColor: 'red'}}>
+            <h3 class="movie__title">
                 {title}
             </h3>
             <h5 class="movie__year">{year}</h5>
